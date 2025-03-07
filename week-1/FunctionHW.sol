8eef99cd4dcd95023e2702b0f321eda452acd357
Marwan Almoghrabi <94754098+Almoghrabi3d@users.noreply.github.com>
2025-03-07 21:41:07 +0300

    Add files via upload
    Hello Everyone 
    This is my first attempt at drafting a contract

---
+++ week-1/FunctionHW.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+
+
+
+contract Helloworld { 
+
+    string private text;
+
+constructor ()
+{
+    
+      text = "Hello world, I am Marwan Almoghrabi";  
+}
+
+function HelloWorld() public view returns (string memory) { 
+    return (text);
+
+ }
+
+function settext(string memory newperson) public {
+    text = newperson;
+}
+
+}
